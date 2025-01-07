a58a2cc4b4395245cdde99103d36d41b2557f590
Abhishek Madiwale <abhishek.madiwale@gmail.com>
2025-01-07 23:52:13 +0530

    Conditional statements with some examples

--- ifElse.cpp
+++ ifElse.cpp
@@ -2,15 +2,60 @@
 using namespace std;
 
 int main() {
-    int n;
-    cout << "Enter the number: " << endl;
-    cin >> n;
 
-    if (n >= 0) {
-        cout << "number is positive";
-    }   
+// Example 1 - finding number is positive or not.
+
+    // int n;
+    // cout << "Enter the number: " << endl;
+    // cin >> n;
+
+    // if (n >= 0) {
+    //     cout << "number is positive";
+    // }   
+    // else {
+    //     cout << "Number is not positive";
+    // }
+    // return 0;
+
+// Checking if the candidate is eligible for voting or not.
+    // int age;
+    // cout << "Enter your age: " << endl;
+    // cin >> age;
+
+    // if (age >= 18) {
+    //     cout << "Candidate eligible for voting";
+    // }
+    // else {
+    //     cout << "Candidate is not eligible for voting";
+    // }
+    // return 0;
+
+// check if numbers is even or odd
+    // int num;
+    // cout << "Enter the number: ";
+    // cin >> num;
+
+    // if (num % 2 == 0) {
+    //     cout << "number is even\n";
+    // }
+    // else {
+    //     cout <<"Number is odd";
+    // }
+    // return 0;
+
+// Students grade printing with using logical operators
+    int marks;
+    cout << "Enter marks: ";
+    cin >> marks;
+
+    if (marks >=90) {
+        cout << "Grade A\n";
+    }
+    else if (marks >= 80 && marks < 90) {
+        cout << "Grade B\n";
+    }
     else {
-        cout << "Number is not positive";
+        cout << "Grade C";
     }
     return 0;
 }
