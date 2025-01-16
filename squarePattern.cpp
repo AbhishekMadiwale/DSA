f8fef6e0c5c86bf3c9c5b3c3238163c9d9716e81
Abhishek Madiwale <abhishek.madiwale@gmail.com>
2025-01-16 08:31:31 +0530

    same problem printing characters instead of numbers

--- squarePattern.cpp
+++ squarePattern.cpp
@@ -3,19 +3,19 @@
 #include<iostream>
 using namespace std;
 
-int main () {
-    int n;
-    cout << "Enter the number: ";
-    cin >> n;
+// int main () {
+//     int n;
+//     cout << "Enter the number: ";
+//     cin >> n;
 
-    for (int i=1; i <= n ; i++) {
-        for (int j = 1; j <= n; j++) {
-            cout << j << " ";
-        }
-        cout << endl;
-    }
-    return 0;
-}
+//     for (int i=1; i <= n ; i++) {
+//         for (int j = 1; j <= n; j++) {
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
 
 // the output should is 
 // Enter the number: 5
@@ -24,3 +24,21 @@ int main () {
 // 1 2 3 4 5
 // 1 2 3 4 5
 // 1 2 3 4 5
+
+// Same problem printing characters
+
+int main () {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << n << endl;
+    for (int i = 1; i <= n ; i++) {
+        char ch = 'A';
+        for (int j = 1; j <= n; j++) {
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
