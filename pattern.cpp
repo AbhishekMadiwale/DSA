69994ee8d1ba4e50b2ac7d5a6202481bc2f2a5d2
Abhishek Madiwale <abhishek.madiwale@gmail.com>
2025-01-16 08:46:50 +0530

    Printing character with the same logic

--- pattern.cpp
+++ pattern.cpp
@@ -2,20 +2,35 @@
 using namespace std;
 
 int main () {
+    // int n;
+    // cout <<"enter a number: ";
+    // cin >>n;
+    // cout << n << endl;
+    // int i;
+    // int j;
+    // int num = 1;
+    // for (i= 1; i <= n; i++) {
+    //     for (j = 1; j <= n; j++) {
+    //         cout << num << ' ';
+    //         num++;
+    //     }
+    //     cout << endl;
+    // }
+    // cout << endl;
+    // return 0;
+
+// printing characters
     int n;
-    cout <<"enter a number: ";
-    cin >>n;
+    cout << "Enter a number: ";
+    cin >> n;
     cout << n << endl;
-    int i;
-    int j;
-    int num = 1;
-    for (i= 1; i <= n; i++) {
-        for (j = 1; j <= n; j++) {
-            cout << num << ' ';
-            num++;
+    char ch = 'A';
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            cout << ch << " ";
+            ch++;
         }
         cout << endl;
     }
-    cout << endl;
     return 0;
 }
